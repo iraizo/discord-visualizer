c7795e6fe18ef1ae2bf19241abc3012af75e05d3
raizo <35413558+iraizo@users.noreply.github.com>
2021-03-04 13:16:46 +0100

    Visualizer commit

---
+++ visualizer.py
@@ -0,0 +1,60 @@
+import redis
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+
+client = redis.Redis(host="127.0.0.1", port=6379,
+                     charset="utf-8", decode_responses=True)
+
+array = client.lrange("messages", 0, -1)
+
+
+def parse_data(array):
+
+    collection = []
+
+    for sentence in array:
+        words = sentence.split()
+
+        if words:
+            collection = collection + words
+
+    return collection
+
+
+def generate_graph():
+    data = parse_data(array)
+
+    counter = Counter(data)
+
+    x = []
+    y = []
+
+    # count appearences of strings
+
+    for ele in counter:
+        if counter[ele] > 50:
+            x.append(ele)
+            y.append(counter[ele])
+
+    # actually generate graph here now
+
+    plot = plt.barh(x, y, 2)
+    plt.xlabel('Messages')
+    plt.ylabel('Occurrences')
+    plt.title('Discord activity')
+
+  #  plot.set_xticklabels(df.index, rotation=45,
+   #                      rotation_mode='anchor', ha='right')
+
+    plt.show()
+
+
+def search(value):
+    matching = [s for s in array if value in s]
+
+    print(matching)
+    print("Occurrences: " + str(len(matching)))
+
+
+generate_graph()
